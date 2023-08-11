6549f122fcdf68ae98855464ca7b9a2329a8da2f
MaryaLauraRS <117856098+MaryaLauraRS@users.noreply.github.com>
2023-08-11 14:03:39 -0400

    PTAC2-main

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import { Exemplo } from './Exemplo';
-import { Dev } from './Dev';
+import {Dev} from './Dev';
 export default function App() {
-    const listaAlunos = ["Marya Laura, 16 anos, Mora em Nova Adradina", "Maria Eduarda, 17 anos, Mora em Nova Andradina", "Maria Fernanda, 15 anos, Mora em Nova Andradina"];
+    const lista = ["Marya Laura, 16 anos, Mora em Nova Adradina", "Maria Eduarda, 17 anos, Mora em Nova Andradina", "Maria Fernanda, 15 anos, Mora em Nova Andradina"];
     return (
       <div>
-        <h1>Gatinho</h1>
-    <p>───▄█▄█▄─────────────▄▄</p>
-    <p>───█████▄▄▄▄───────▄▀</p>
-    <p>─────▀██▀▀████▄───▄▀</p>
-    <p>────▄█▀▄██▄████▄─▄█</p>
-    <br></br><br></br>
-   
-    <Exemplo lista={listaAlunos} />
+    <Exemplo lista={lista} />
     <Dev nome="Marya"/>
   </div>
     );
--- src/Exemplo.jsx
+++ src/Exemplo.jsx
@@ -1,16 +1,7 @@
-export function Exemplo({lista}, {nome}){
-    return(
-        <div>
-  <p>──██████──────██████</p>
-  <p>─████████▄▄▄▄████████</p>
-  <p>──██████████████████</p>
-  <p>───████████████████</p>
-  <p>─────████████████</p>
-  <p>───────████████</p>
-  <p>─────────████</p>
-  <p>──────────██</p>
-  <br></br><br></br>
-<h1>Lista de alunos</h1>
+export function Exemplo({lista}){
+    return( 
+      <div> 
+    <h1>Lista de alunos</h1>
   {lista.map(aluno => (
             <ul>
         <li>{aluno}</li>
